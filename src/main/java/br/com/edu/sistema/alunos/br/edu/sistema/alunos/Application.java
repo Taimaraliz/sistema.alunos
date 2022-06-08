9e52e21530c972c992dc23831ac78d440b1ede9f
Taimara Liz <tailizadm@gmail.com>
2022-06-08 19:56:05 -0300

    Commit inicial sistema de alunos - crud

---
+++ src/main/java/br/com/edu/sistema/alunos/br/edu/sistema/alunos/Application.java
@@ -0,0 +1,13 @@
+package br.com.edu.sistema.alunos.br.edu.sistema.alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
